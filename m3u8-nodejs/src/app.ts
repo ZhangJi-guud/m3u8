d34ab3cd75dea6e9abf2f60ca067b9dcf9872803
ZhangJi-guud <zhang.ji@guud.company>
2023-04-16 18:40:17 +0800

    getM3u8

--- m3u8-nodejs/src/app.ts
+++ m3u8-nodejs/src/app.ts
@@ -1,11 +1,18 @@
 import express, { Express, Request, Response } from 'express';
 import { getYtDlpM3u8Url } from './ytDlp';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app: Express = express();
 //app.use(bodyParser());
 //app.use(bodyParser.urlencoded());
+
+
+app.use(cors({ allowedHeaders: 'Content-Type, Cache-Control' }));
+app.options('*', cors());  // enable pre-flight
+
 app.use(bodyParser.json());
+//app.use(cors);
 
 const PORT: number = 3001;
 
@@ -13,7 +20,7 @@ app.post('/getM3u8', (req: Request, res: Response): void => {
 
     //let videoId: string = "E0CvY6GLy3g";
     console.log(req.body);
-    const ganJingUrl = req.body.ganJingUrl;
+    const ganJingUrl = req.body?.ganJingUrl;
     let m3u8Url: string = "";
 
     getYtDlpM3u8Url(ganJingUrl)
@@ -25,11 +32,12 @@ app.post('/getM3u8', (req: Request, res: Response): void => {
             console.error(error);
         });
 
-    res.send({m3u8Url});
+    res.send({ m3u8Url });
 });
 
 
 app.use('/', (req: Request, res: Response): void => {
+    console.log("/", req.body);
     res.send('Hello world!');
 });
 
